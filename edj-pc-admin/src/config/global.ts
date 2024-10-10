f1e0844414b81fb08fc019b9df058b0c52f756b5
A.E. <570796406@qq.com>
2024-10-10 22:43:28 +0800

    Egret DaoJia frontend init

---
+++ edj-pc-admin/src/config/global.ts
@@ -0,0 +1,4 @@
+export const prefix = 'yldj'
+export const TOKEN_NAME = 'xzb'
+export const AUTHORIZATION_ACCESS_TOKEN = 'AuthorizationAccessToken'
+export const AUTHORIZATION_REFRESH_TOKEN = 'AuthorizationRefreshToken'
---
+++ edj-pc-admin/src/store/modules/user.ts
@@ -0,0 +1,61 @@
+import { defineStore } from 'pinia'
+import {
+  AUTHORIZATION_ACCESS_TOKEN,
+  AUTHORIZATION_REFRESH_TOKEN
+} from '@/config/global'
+import { store, usePermissionStore } from '@/store'
+
+// 初始化用户信息
+const InitUserInfo = {
+  roles: []
+}
+
+export const useUserStore = defineStore('user', {
+  // 定义用户 Store
+  state: () => ({
+    authorizationAccessToken:
+      localStorage.getItem(AUTHORIZATION_ACCESS_TOKEN) || '111',
+    authorizationRefreshToken:
+      localStorage.getItem(AUTHORIZATION_REFRESH_TOKEN) || '222',
+    userInfo: { ...InitUserInfo }
+  }),
+  getters: {
+    roles: (state) => {
+      return state.userInfo?.roles
+    }
+  },
+  actions: {
+    async login(accessToken: string, refreshToken: string) {
+      this.authorizationAccessToken = accessToken
+      this.authorizationRefreshToken = refreshToken
+      localStorage.setItem(AUTHORIZATION_ACCESS_TOKEN, accessToken)
+      localStorage.setItem(AUTHORIZATION_REFRESH_TOKEN, refreshToken)
+    },
+    async setUserInfo(data: any) {
+      this.userInfo = data
+    },
+    async logout() {
+      localStorage.removeItem(AUTHORIZATION_ACCESS_TOKEN)
+      localStorage.removeItem(AUTHORIZATION_REFRESH_TOKEN)
+      this.authorizationAccessToken = ''
+      this.authorizationRefreshToken = ''
+      this.userInfo = { ...InitUserInfo }
+    },
+    async removeToken() {
+      this.authorizationAccessToken = ''
+      this.authorizationRefreshToken = ''
+    }
+  },
+  persist: {
+    afterRestore: (ctx) => {
+      if (ctx.store.roles && ctx.store.roles.length > 0) {
+        const permissionStore = usePermissionStore()
+        permissionStore.initRoutes(ctx.store.roles)
+      }
+    }
+  }
+})
+
+export function getUserStore() {
+  return useUserStore(store)
+}
