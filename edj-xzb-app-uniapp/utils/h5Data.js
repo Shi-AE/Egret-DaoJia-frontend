a055a1646634e5c8ca333eedec7435d694077a54
A.E. <570796406@qq.com>
2025-04-24 20:31:33 +0800

    修改默认信息

--- edj-xzb-app-uniapp/utils/h5Data.js
+++ edj-xzb-app-uniapp/utils/h5Data.js
@@ -1,8 +1,8 @@
 // 该文件是，h5情况下的运行环境，对应的一些需要的数据
 export const data = {
-    longitude: 116.4343,
-    latitude: 40.008,
-    city: '北京市',
-    cityCode: '010',
-    address: '北京黑马程序员'
+    longitude: 120.202202,
+    latitude: 31.787336,
+    city: '无锡市',
+    cityCode: '无锡市',
+    address: '江苏省无锡市江阴市月城镇协和大桥'
 }
